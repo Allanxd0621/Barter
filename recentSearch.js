cc9a136d332d2737d2ef1375677e9058d9db5921
Allandino Manalo <allandinomanalo@gmail.com>
2026-05-27 21:09:43 +0800

    Update recentSearch.js

--- recentSearch.js
+++ recentSearch.js
@@ -1,8 +1,19 @@
 const searchBar = document.getElementById("searchBar");
 const recentSearches = document.querySelector(".recentSearches");
 
-searchBar.addEventListener("click" , function(){
+searchBar.addEventListener("focus" , function(){
 
 recentSearches.classList.add("recentSearchesVisible");
+
+});
+
+//exit sa recent searches
+
+
+const exitBtn = document.getElementById("exitRecent");
+
+exitBtn.addEventListener("click" , function(){
+
+recentSearches.classList.remove("recentSearchesVisible");
 
 });
